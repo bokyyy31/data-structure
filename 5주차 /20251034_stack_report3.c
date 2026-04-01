9d5302d44a6c35483d33f6626bb646a97d213156
bokyyy31 <ddddfh@naver.com>
2026-04-01 15:06:02 +0900

    _

--- 5주차 /20251034_stack_report3.c
+++ 5주차 /20251034_stack_report3.c
@@ -1,1 +1,118 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
+#define MAX 100
+
+typedef struct {
+    int data[MAX];
+    int top;
+} Stack;
+
+void initStack(Stack *s) {
+    s->top = -1;
+}
+
+int isEmpty(Stack *s) {
+    return s->top == -1;
+}
+
+void push(Stack *s, int value) {
+    s->data[++(s->top)] = value;
+}
+
+int pop(Stack *s) {
+    if (isEmpty(s)) return '\0';
+    return s->data[(s->top)--];
+}
+
+int peek(Stack *s) {
+    if (isEmpty(s)) return '\0';
+    return s->data[s->top];
+}
+
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return -1;
+}
+
+int infixToPostfix(char *infix, char *postfix) {
+    Stack s;
+    initStack(&s);
+    int j = 0;
+
+    for (int i = 0; infix[i] != '\0'; i++) {
+        char ch = infix[i];
+
+        if (isalpha(ch) || isdigit(ch)) {
+            postfix[j++] = ch;
+        }
+        else if (ch == '(') {
+            push(&s, ch);
+        }
+        else if (ch == ')') {
+            while (!isEmpty(&s) && peek(&s) != '(') {
+                postfix[j++] = pop(&s);
+            }
+            pop(&s); 
+        }
+        else {
+            while (!isEmpty(&s) && precedence(peek(&s)) >= precedence(ch)) {
+                postfix[j++] = pop(&s);
+            }
+            push(&s, ch);
+        }
+    }
+
+    while (!isEmpty(&s)) {
+        postfix[j++] = pop(&s);
+    }
+    postfix[j] = '\0';
+
+
+    Stack calc;
+    initStack(&calc);
+
+    for (int i = 0; postfix[i] != '\0'; i++) {
+        char ch = postfix[i];
+
+        if (isdigit(ch)) {
+            push(&calc, ch - '0'); 
+        }
+        else {
+            int b = pop(&calc);
+            int a = pop(&calc);
+            int result;
+
+            switch (ch) {
+                case '+': result = a + b; break;
+                case '-': result = a - b; break;
+                case '*': result = a * b; break;
+                case '/': result = a / b; break;
+                default:
+                    printf("잘못된 연산.\n");
+                    exit(1);
+            }
+            push(&calc, result);
+        }
+    }
+
+    return pop(&calc); 
+}
+
+int main(void) {
+    char infix[MAX];
+    char postfix[MAX];
+    int result;
+
+    printf("중위표기식 입력 : ");
+    scanf("%s", infix);
+
+    result = infixToPostfix(infix, postfix);
+    printf("후위표기식 : %s\n", postfix);
+    printf("후위표기식 계산 결과 : %d\n", result);
+
+    return 0;
+}
