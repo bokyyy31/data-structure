7c7905c8edce7dff38cec03b0d99585615c3918d
bokyyy31 <ddddfh@naver.com>
2026-04-01 14:12:39 +0900

    _

--- 3주차/20251034_intro_report1.c
+++ 3주차/20251034_intro_report1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main() {
+    int scores[10];   
+    int sum = 0;      
+    double average;   
+
+    srand(time(NULL));  
+
+    
+    for (int i = 0; i < 10; i++) {
+        scores[i] = rand() % 101; 
+        sum += scores[i];
+        printf("학생 %d : %d\n", i + 1, scores[i]);
+    }
+
+    average = (double)sum / 10; 
+    printf("\n평균 : %.2f\n", average);
+
+    return 0;
+}
