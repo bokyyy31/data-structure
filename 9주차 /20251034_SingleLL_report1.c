55aa19ee3dfaa088baf10e002115916b835d7110
bokyyy31 <ddddfh@naver.com>
2026-04-30 21:44:20 +0900

    .

--- 9주차 /20251034_SingleLL_report1.c
+++ 9주차 /20251034_SingleLL_report1.c
@@ -1,1 +1,212 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* insertFirst(Node* head, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (head == NULL) {
+        return newNode;
+    }
+
+    newNode->next = head;
+    return newNode;
+}
+
+Node* insertLast(Node* head, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (head == NULL) {
+        return newNode;
+    }
+
+    Node* current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = newNode;
+
+    return head;
+}
+
+void insertAfterValue(Node* head, int targetValue, int newData) {
+    Node* current = head;
+
+    while (current != NULL) {
+        if (current->data == targetValue) {
+            Node* newNode = (Node*)malloc(sizeof(Node));
+            newNode->data = newData;
+            newNode->next = current->next;
+            current->next = newNode;
+            return;
+        }
+        current = current->next;
+    }
+
+    printf("%d 노드 없음.\n", targetValue);
+}
+
+Node* deleteHead(Node* head) {
+    if (head == NULL) {
+        printf("비어있음.\n");
+        return NULL;
+    }
+
+    Node* temp = head;
+    head = head->next;
+    free(temp);
+
+    return head;
+}
+
+Node* deleteLast(Node* head) {
+    if (head == NULL) {
+        printf("비어있음.\n");
+        return NULL;
+    }
+
+    if (head->next == NULL) {
+        free(head);
+        return NULL;
+    }
+
+    Node* current = head;
+    while (current->next->next != NULL) {
+        current = current->next;
+    }
+
+    free(current->next);
+    current->next = NULL;
+
+    return head;
+}
+
+Node* deleteNode(Node* head, int targetValue) {
+    if (head == NULL) {
+        printf("비어있음.\n");
+        return NULL;
+    }
+
+    if (head->data == targetValue) {
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+
+    Node* current = head;
+    while (current->next != NULL && current->next->data != targetValue) {
+        current = current->next;
+    }
+
+    if (current->next == NULL) {
+        printf("%d 노드 없음.\n", targetValue);
+        return head;
+    }
+
+    Node* temp = current->next;
+    current->next = current->next->next;
+    free(temp);
+
+    return head;
+}
+
+void printList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+int searchValue1(Node* head, int val) {
+    Node* current = head;
+    int pos = 1;
+
+    while (current != NULL) {
+        if (current->data == val) {
+            return pos;
+        }
+        current = current->next;
+        pos++;
+    }
+
+    return -1;
+}
+
+void searchValue2(Node* head, int val) {
+    Node* current = head;
+
+    while (current != NULL) {
+        if (current->data == val) {
+            if (current == head) {
+                printf("앞 노드 없음\n");
+            } else {
+                Node* prev = head;
+                while (prev->next != current) {
+                    prev = prev->next;
+                }
+                printf("앞 노드 %d\n", prev->data);
+            }
+
+            if (current->next == NULL) {
+                printf("뒤 노드 없음\n");
+            } else {
+                printf("뒤 노드 %d\n", current->next->data);
+            }
+            return;
+        }
+        current = current->next;
+    }
+
+    printf("%d 없음.\n", val);
+}
+
+void freeList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
+
+int main() {
+    Node* head = NULL;
+
+    head = insertFirst(head, 30);
+    head = insertFirst(head, 20);
+    head = insertFirst(head, 10);
+    head = insertLast(head, 70);
+    head = insertLast(head, 80);
+    head = insertLast(head, 90);
+    insertAfterValue(head, 30, 40);
+    insertAfterValue(head, 40, 50);
+    insertAfterValue(head, 50, 60);
+
+    head = deleteHead(head);
+    head = deleteLast(head);
+    head = deleteNode(head, 40);
+    head = deleteNode(head, 70);
+    head = deleteNode(head, 100);
+
+    printList(head);
+
+    int pos = searchValue1(head, 30);
+    printf("30의 위치는 %d번째\n", pos);
+
+    searchValue2(head, 30);
+
+    freeList(head);
+
+    return 0;
+}
